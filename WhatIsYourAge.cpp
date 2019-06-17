dd60a5b6a1b98ac3327b70aa5404e898f86ce693
smyousuf14 <44485548+smyousuf14@users.noreply.github.com>
2019-06-17 16:05:21 -0400

    Create WhatIsYourAge.cpp

---
+++ WhatIsYourAge.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age;
+    
+    cout << "What is your age:";
+    cin >> age;
+    
+    cout << "Your age is " << age;
+    
+    return 0;
+}
