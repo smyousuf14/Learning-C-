e78de44906bb8ab26ce107305901c68503019fa3
smyousuf14 <44485548+smyousuf14@users.noreply.github.com>
2019-06-22 22:41:46 -0400

    Create C++Array.cpp

---
+++ C++Array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number[5] = {3, 5, 7, 8};
+    int indexNumber;
+    
+    for(indexNumber; indexNumber < 4; indexNumber++)
+    {
+        cout << number[indexNumber] << "\n";
+    }
+}
