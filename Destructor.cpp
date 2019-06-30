16430ef1d827c8960ac2114d70ee1061989c1659
smyousuf14 <44485548+smyousuf14@users.noreply.github.com>
2019-06-29 20:55:15 -0400

    Create Destructor.cpp

---
+++ Destructor.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+class Employee
+{
+    public:
+        
+        Employee()
+        {
+            cout << "Constructor invoked" << endl;
+        }
+        
+        ~Employee()
+        {
+            cout << "Destructor invoked" << endl;
+        }
+};
+
+
+int main()
+{
+    Employee e1;
+    
+}
