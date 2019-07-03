acdb66964bf1e318053bfb5d6e43d85185a7f4fe
smyousuf14 <44485548+smyousuf14@users.noreply.github.com>
2019-07-03 12:57:04 -0400

    Create overloadingOperatorsExample.cpp

---
+++ Overloading/overloadingOperatorsExample.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+class Cal 
+{
+    private:
+        int sum;
+    
+    public:
+        
+        Cal()
+        {
+            sum = 0;
+        }
+        
+        void add(int a, int b)
+        {
+            sum = a + b;
+        }
+        
+        void add(int a, int b, int c)
+        {
+            sum = a + b + c;
+        }
+        
+        void operator ++()
+        {
+            sum = sum + 1;
+        }
+        
+        int getSum()
+        {
+            return sum;
+        }
+};
+
+int main()
+{
+    Cal c1;
+    c1.add(5, 6);
+    cout << c1.getSum() << endl;
+    
+    c1.add(1, 6, 9);
+    cout << c1.getSum() << endl;
+    
+    ++c1;
+    cout << c1.getSum() << endl;
+    return 0;
+    
+}
