f9a5296808d7313620cf4bb90d90ac423963259a
smyousuf14 <44485548+smyousuf14@users.noreply.github.com>
2019-07-15 11:05:42 -0400

    Create example.cpp

---
+++ String/example.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string s1 = "Hello";
+    char chr[] = {'C','A', 'T'};
+    
+    string s2 = string(chr);
+    cout << s1 << endl;
+    cout << s2 << endl;
+    
+    return 0;
+    
+}
